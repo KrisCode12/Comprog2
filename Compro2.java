44c0e116c3ec2f1ba93c33a4d624f6d56e2b740c
Cojhe_loc <92619185+KrisCode12@users.noreply.github.com>
2021-11-27 10:44:30 +0800

    Update Compro2.java

--- Compro2.java
+++ Compro2.java
@@ -4,11 +4,11 @@ class Main {
 	
 	public static void main(String []args) {
 		
-		Scanner sc = new Scanner(System.in);
+		Scanner scan = new Scanner(System.in);
 		int num = 0;
 		
 	System.out.print("Enter an integer: ");
-		num = sc.nextInt();
+		num = scan.nextInt();
 		
 		showNumberPlus10(num);
 		showNumberPlus100(num);

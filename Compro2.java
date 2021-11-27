aaa66d2ea0fa62f89c8cfc228e7ec66a3b1d5850
Cojhe_loc <92619185+KrisCode12@users.noreply.github.com>
2021-11-27 10:43:41 +0800

    Create Compro2.java

---
+++ Compro2.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+class Main {
+	
+	public static void main(String []args) {
+		
+		Scanner sc = new Scanner(System.in);
+		int num = 0;
+		
+	System.out.print("Enter an integer: ");
+		num = sc.nextInt();
+		
+		showNumberPlus10(num);
+		showNumberPlus100(num);
+		showNumberPlus1000(num);
+	}
+		
+	static void showNumberPlus10(int num) {
+		System.out.println(num + " plus 10 is " + (num + 10) + ".");
+	}
+	static void showNumberPlus100(int num) {
+		System.out.println(num + " plus 100 is " + (num + 100) + ".");
+	}
+	static void showNumberPlus1000(int num) {
+		System.out.println(num + " plus 1000 is " + (num + 1000) + ".");
+	}
+  }
